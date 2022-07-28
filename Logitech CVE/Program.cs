244220e470f5a875ae94445d13ab1c3f95870218
fetix <78823241+feticks@users.noreply.github.com>
2022-07-28 18:30:05 +0100

    Add files via upload

---
+++ Logitech CVE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace CVE {
+    class Program {
+        static void Main(string[] args) {
+            if (!Mouse.Open()) Console.WriteLine("Failed");
+            else {
+                Console.WriteLine("Success\n\n");
+
+                for (int i = 0; i < 100; i++) {
+                    Thread.Sleep(20);
+                    Console.WriteLine("[+] Moving mouse\n");
+                    Mouse.Move(0, 5, 0, 0);
+                }
+
+                Mouse.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Logitech CVE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MouseCSharpCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Project-OS.org")]
+[assembly: AssemblyProduct("MouseCSharpCode")]
+[assembly: AssemblyCopyright("Copyright © Project-OS.org 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c28f4cd1-76db-4e1f-9cd8-af091ac13065")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
